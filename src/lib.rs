4a9a148ab8ea4d582420cd36e1d884c9ba7cd5fc
Henning Timm <henning.timm@tu-dortmund.de>
2019-04-25 15:40:52 +0200

    Formatted

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! This crate offers rust implementations of simple and twisted tabulation hashing for 32-bit integer values.
-//! 
+//!
 //! # Example:
-//! 
+//!
 //! ```rust
 //! use tab_hash::Tab32Simple;
 //!
@@ -13,7 +13,7 @@
 //!     }
 //! }
 //! ```
-//! 
+//!
 
 use rand;
 
@@ -27,9 +27,8 @@ fn byte_chunks(x: u32) -> [u8; 4] {
     ]
 }
 
-
 pub struct Tab32Simple {
-    table: [[u32; 256]; 4]
+    table: [[u32; 256]; 4],
 }
 
 impl Tab32Simple {
@@ -40,9 +39,9 @@ impl Tab32Simple {
     }
 
     pub fn with_table(table: [[u32; 256]; 4]) -> Self {
-        Tab32Simple{table}
+        Tab32Simple { table }
     }
-    
+
     /// Generate a table of 32bit uints for simple tabulation hashing
     fn initialize_table() -> [[u32; 256]; 4] {
         let table: [[u32; 256]; 4] =
@@ -53,7 +52,7 @@ impl Tab32Simple {
     pub fn get_table(&self) -> [[u32; 256]; 4] {
         self.table
     }
-    
+
     /// Simple tabulation hash for a 32bit integer number.
     pub fn hash(&self, x: u32) -> u32 {
         let mut h: u32 = 0; // initialize hash values as 0
@@ -65,10 +64,8 @@ impl Tab32Simple {
     }
 }
 
-
-
 pub struct Tab32Twisted {
-    table: [[u64; 256]; 4]
+    table: [[u64; 256]; 4],
 }
 
 impl Tab32Twisted {
@@ -79,9 +76,9 @@ impl Tab32Twisted {
     }
 
     pub fn with_table(table: [[u64; 256]; 4]) -> Self {
-        Tab32Twisted{table}
+        Tab32Twisted { table }
     }
-    
+
     /// Generate a table of 64bit uints for twistedtabulation hashing
     fn initialize_table() -> [[u64; 256]; 4] {
         let table: [[u64; 256]; 4] =
@@ -103,9 +100,7 @@ impl Tab32Twisted {
         let c = chunks[3] ^ (h & 0xFF) as u8;
         h ^= self.table[3][c as usize];
         h = h.overflowing_shr(32).0;
-    
+
         h as u32
     }
-    
 }
-
